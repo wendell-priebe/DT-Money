a023d1ae849baca0a5be240cd5dd86a34865f673
Wendell Priebe <wendel_oliveira94@hotmail.com>
2021-08-04 16:36:25 -0300

    isActiveColor fix

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -71,7 +71,7 @@ export const RadioBox = styled.button<RadioBoxProps>`
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
 
-  background: ${(props)=> props.activeColor
+  background: ${(props)=> props.isActive
     ? transparentize(0.8 , colors[props.activeColor]) 
     : 'transparent'
     
